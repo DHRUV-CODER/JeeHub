305fd4f7fa22b079e8c232dc84d05653b58aace5
Dhruv <dhruvnation15@gmail.com>
2024-02-17 15:57:41 +0300

    new ui -v1

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div>
-      <div className="px-1 py-2 flex items-center justify-center">
+      <div className="px-1 py-4 flex items-center justify-center">
         <h1 className="mb-4 font-extrabold leading-none tracking-tight text-2xl md:text-5xl lg:text-6xl text-stone-300">
           Your only{" "}
           <mark className="px-2 text-black rounded bg-orange-500">Hub</mark> for
