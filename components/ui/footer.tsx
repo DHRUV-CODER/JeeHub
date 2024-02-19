7306845999c688602cc0d9337bf4a1cbadce903c
Dhruv <dhruvnation15@gmail.com>
2024-02-19 19:03:01 +0300

    bruh

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
             . All Rights Reserved.
           </span>
           <span className="text-sm text-stone-400 font-bold sm:text-center flex items-center justify-center gap-x-2 text-muted-foreground">
-            This site is made for personal use only , We don't use it in means
+            This site is made for personal use only , We don&apos;t use it in means
             of monetization. <CircleDollarSign fill="black" />
           </span>
 
